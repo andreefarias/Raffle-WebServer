5f312c0b41188ca2b9d398bc9102a9a22207c146
André Farias <maalkdown@gmail.com>
2022-06-03 23:29:02 -0300

    Update project

---
+++ README.md
@@ -0,0 +1,32 @@
+# Raffle Web Server | Servidor Web de Sorteio
+Web page with a raffle system, developed in Kotlin, JavaScript, HTML and CSS.
+
+The files and folder this project:
+
+* __src/main/Kotlin/KotlinApplication/KotlinApplication.kt__: Run project server
+* __src/main/resources/static/js/script.js__: Backend fuctions
+* __src/main/resources/static/index.html__: Frontend fuctions
+* __src/main/resources/static/css/style.css__: Styles on frontend
+* __src/main/resources/static/application.properties__: Define the port of the server (localhost)
+
+Software:
+```
+IntelliJ IDEA
+```
+
+Build the server:
+```
+KotlinApplication.kt
+```
+
+Run the server:
+```
+KotlinApplication.kt
+```
+
+Go to browser:
+```
+Type on search bar: localhost:8080 (default port)
+```
+
+![raffle](https://user-images.githubusercontent.com/73314261/170289575-b327463c-864b-40a3-85eb-b62f224b8c75.gif)
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,107 @@
+let result = document.getElementById("result");
+let input = document.getElementById("input");
+let names = document.getElementById("names");
+let divResult = document.getElementById("divResult");
+let mainContainer = document.getElementById("mainContainer");
+let resultContainer = document.getElementById("resultContainer");
+let errorName = document.getElementById("errorName");
+let buttonAddName = document.getElementById("buttonAddName");
+let checkBox = document.getElementById("checkbox");
+let buttonSorteio = document.getElementById("buttonSorteio");
+
+// Add name
+function addName() {
+    let valor = input.value;
+
+    if (valor === "" || valor === " ") {
+        errorName.innerHTML = "*PREENCHA O CAMPO COM O NOME*";
+        errorName.style.opacity = "100%";
+        errorName.style.visibility = "visible";
+    } else {
+        names.innerHTML = "<div class='nameCFG'><p class='nameP'></p><button class='buttonRemoveName'>REMOVER</button></div>" + names.innerHTML;
+
+        let nameP = document.getElementsByClassName("nameP")[0];
+        nameP.innerHTML = input.value;
+
+        input.value = " ";
+
+        errorName.style.visibility = "hidden";
+        errorName.style.opacity = "0%";
+
+        valor = JSON.stringify(names.innerHTML);
+        localStorage.setItem("valor", valor);
+    }
+
+    buttonAddName.addEventListener("click", onload());
+}
+
+// Remove name
+function removeName() {
+    let buttonRemove = this;
+    let remove = buttonRemove.parentElement;
+    remove.remove();
+
+    valor = JSON.stringify(names.innerHTML);
+    localStorage.setItem("valor", valor);
+}
+
+let nameP = document.getElementsByClassName("nameP");
+
+// Sorteia
+function sorteio() {
+    let valorName = names.innerHTML;
+
+    if (valorName == "" || valorName == " ") {
+        errorName.innerHTML = "*PREENCHA O CAMPO COM O NOME*";
+        errorName.style.opacity = "100%";
+        errorName.style.visibility = "visible";
+    } else if (names.children.length < 2) {
+        errorName.innerHTML = "*PREENCHA O CAMPO COM AO MENOS DOIS NOMES*";
+        errorName.style.opacity = "100%";
+        errorName.style.visibility = "visible";
+    } else {
+        let numP = nameP.length;
+        let numS = Math.floor(Math.random() * numP);
+        result.innerHTML = nameP[numS].innerHTML;
+
+        divResult.style.opacity = "100%";
+        divResult.style.visibility = "visible";
+        mainContainer.style.filter = "blur(5px)";
+
+        let divSize = window.getComputedStyle(mainContainer, null).getPropertyValue("height");
+        resultContainer.style.height = divSize;
+
+        errorName.style.visibility = "hidden";
+        errorName.style.opacity = "0%";
+    }
+
+    buttonSorteio.addEventListener("click", check());
+}
+
+// Verifica
+function check() {
+    if (checkBox.checked == true) {
+        names.innerHTML = "";
+        valor = JSON.stringify(names.innerHTML);
+        localStorage.setItem("valor", valor);
+    }
+}
+
+onload = function () {
+    let dadoName = localStorage.getItem("valor");
+    let dado = JSON.parse(dadoName);
+
+    names.innerHTML = dado;
+    let removeButton = document.getElementsByClassName("buttonRemoveName");
+
+    for (let rB of removeButton) {
+        rB.addEventListener("click", removeName);
+    }
+};
+
+// Botão voltar
+function buttonBack() {
+    divResult.style.opacity = "0%";
+    divResult.style.visibility = "hidden";
+    mainContainer.style.filter = "none";
+}
